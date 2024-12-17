Add rotation for different axes, start fixing the projection

main.py
@@ -2,18 +2,20 @@ import pygame as pg
 import test_donut as td
 import numpy as np
 from math import pi
+from torus import Torus
 
 pg.init()
 
 W, H = 800, 600
-DIST_TO_SCREEN = 10
+DIST_TO_SCREEN = 250
+FRAMES = 15
 CLOCK = pg.time.Clock()
 WHITE = (255, 255, 255)
 screen = pg.display.set_mode((W, H))
 
-c = td.circle(np.array([W/2, H/2, 10]), screen)
+c = td.Circle(np.array([W/2, H/2, 80]))
 c.make_circle()
-
+t = Torus(np.array([W/2, H/2, 80]))
 run = True
 screen.fill(WHITE)
 
@@ -22,12 +24,13 @@ while run:
         if e.type == pg.QUIT:
             run = False
 
-    c.rotate_circle(pi/10)
-    c.draw_circle(DIST_TO_SCREEN/c.z)
+    c.rotate_circle(pi/(2*FRAMES))
+    c.draw_circle(screen, DIST_TO_SCREEN)
 
+    # t.draw_torus(screen)
 
     pg.display.update()
-    CLOCK.tick(20)
+    CLOCK.tick(FRAMES)
     screen.fill(WHITE)
 
 pg.quit()

test_donut.py
@@ -4,45 +4,63 @@ from math import sin, cos, pi
 
 
 
-class circle:
+class Circle:
 
     x: float
     y: float
     z: float
+    center: np.array
     points: list[np.array]
     rad: float
-    surf: pg.Surface
+    num_points: float
 
-    def __init__(self, center : np.array, surf: pg.Surface):
+    def __init__(self, center: np.array):
         self.x = center[0]
         self.y = center[1]
-        self.z = center[2]
-        self.surf = surf
-        self.rad = 50
+        self.dist_from_view = center[2] + 90
+        self.screen_center = np.array([self.x, self.y])
+        self.center = center
+        self.rad = 90
         self.points = []
+        self.num_points = 50
 
     def show_center(self):
         print(self.x, self.y, self.z)
 
     def make_circle(self):
-        for theta in range(0, 30):
-            px = self.rad*cos(theta*(2*pi/30))    
-            py = self.rad*sin(theta*(2*pi/30))
-            self.points.append(np.array([px, py, self.z]))
+        for theta in range(0, self.num_points):
+            px = 90+self.rad*cos(theta*(2*pi/self.num_points))    
+            py = self.rad*sin(theta*(2*pi/self.num_points))
+            self.points.append(np.array([px, py, 0]))
 
-    def draw_circle(self, proj_scalar = 1):
+    def draw_circle(self, surf: pg.Surface, dist_to_screen = 100):
         for p in self.points:
-            proj_x = p[0]*proj_scalar
-            proj_y = p[1]*proj_scalar
-            pg.draw.circle(self.surf, (0, 0, 0), (self.x + proj_x, self.y + proj_y), 2) 
+            proj_scalar = dist_to_screen/(p[2] + self.dist_from_view + dist_to_screen)
+            # print(dist_to_screen, p[2], proj_scalar)
+            proj_p = np.array([p[0]*proj_scalar, 
+                               p[1]*proj_scalar])
+            
+            pg.draw.circle(surf, (0, 0, 0), np.add(self.screen_center, proj_p), 2) 
     
-    def rotate_circle(self, theta, axis=0):
-        rotation_yaxis = np.array([[cos(theta), 0, sin(theta)], 
+    def rotate_circle(self, theta, axis='y'):
+        match axis:
+            case 'x':
+                rotation_axis = np.array([[1, 0, 0],
+                                          [0, cos(theta), -sin(theta)],
+                                          [0, sin(theta), cos(theta)]])
+            case 'y':
+                rotation_axis = np.array([[cos(theta), 0, sin(theta)], 
                                    [0, 1, 0], 
                                    [-sin(theta), 0, cos(theta)]])
+            case 'z':
+                rotation_axis = np.array([[cos(theta), -sin(theta), 0], 
+                                   [sin(theta), cos(theta), 0], 
+                                   [0, 0, 1]])
+        
         
         for i in range(len(self.points)):
-            self.points[i] = np.matmul(rotation_yaxis, self.points[i])
+            self.points[i] = np.matmul(rotation_axis, self.points[i])
+    
 
 
 